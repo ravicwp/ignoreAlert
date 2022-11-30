125a4c217cd413ba9b74e0c61c822b4fb1472b3d
ravicwp <109152895+ravicwp@users.noreply.github.com>
2022-11-30 17:42:16 +0530

    Update Aws_S3_Bucket.tf

--- AWS/Aws_S3_Bucket.tf
+++ AWS/Aws_S3_Bucket.tf
@@ -7,3 +7,35 @@ IgnorePublicAcls = true
 RestrictPublicBuckets = true
 }
 }
+
+resource "aws_s3_bucket" "nov2" {
+  bucket = "30Nov1"
+  PublicAccessBlock{
+BlockPublicAcls = true
+BlockPublicPolicy = true
+IgnorePublicAcls = true
+RestrictPublicBuckets = true
+    
+     versioning {
+    mfa_delete =false
+    enabled = false
+    
+  }
+}
+}
+
+resource "aws_s3_bucket" "nov3" {
+  bucket = "30Nov1"
+  PublicAccessBlock{
+BlockPublicAcls = true
+BlockPublicPolicy = true
+IgnorePublicAcls = true
+RestrictPublicBuckets = true
+    
+     versioning {
+    mfa_delete =false
+    enabled = false
+    
+  }
+}
+}
