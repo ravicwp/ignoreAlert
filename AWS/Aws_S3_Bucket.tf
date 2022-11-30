408e52338f2cc4a927423e10aa7b0db60d94ef27
ravicwp <109152895+ravicwp@users.noreply.github.com>
2022-11-30 07:36:59 +0530

    Update Aws_S3_Bucket.tf

--- AWS/Aws_S3_Bucket.tf
+++ AWS/Aws_S3_Bucket.tf
@@ -1,5 +1,11 @@
 resource "aws_s3_bucket" "nov1" {
   bucket = "30Nov1"
+  "PublicAccessBlock":{
+"BlockPublicAcls":true
+"BlockPublicPolicy":true
+"IgnorePublicAcls":true
+"RestrictPublicBuckets":true
+}
 }
 
 resource "aws_s3_bucket" "Nov2" {
